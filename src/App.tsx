b55cf252505c7a5005a04b4732039171990b1a5b
astersailor <142462619+AsterSailor@users.noreply.github.com>
2025-06-25 17:02:03 +0000

    save for now (there are stacked vectors)

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,9 @@ import { useState } from "react";
 import { useRef, useEffect } from "react";
 
 let initialList: number[] = [0, 1, 4, 9];
-let initialVector: string[] = ["pie", "twig", "lemon"];
-let miniList: any[] = [2, "red", "roses"];
+let initialVector: string[] = ["lemon", "pie", "twig"];
+
+let rosieVec: string[] = ['r','o','s','e']
 
 let listObj: {
   type: any;
@@ -22,6 +23,16 @@ let vecObj: {
   value: initialVector,
 };
 
+let rosieObj: {
+  type: any;
+  value: any;
+} = {
+  type: "vector",
+  value: rosieVec,
+};
+
+let miniList: any[] = [2, rosieObj, rosieObj];
+
 let miniListObj: {
   type: any;
   value: any;
@@ -30,7 +41,7 @@ let miniListObj: {
   value: miniList,
 };
 
-let nestedVec: any[] = [8, 4, miniListObj, 7];
+let nestedVec: any[] = [miniListObj, 4, miniListObj, 7];
 
 let nestedVecObj: {
   type: any;
@@ -52,47 +63,49 @@ let initialRows: {
   { name: "str_vec", value: vecObj },
 ];
 
+
 export function arrow(
   ctx: CanvasRenderingContext2D,
   x_val: number,
   y_val: number,
-  dir: string
+  dir: string,
+  len: number
 ) {
   if (dir == "horizontal") {
     ctx.beginPath();
     ctx.setLineDash([]);
     ctx.moveTo(x_val, y_val);
-    ctx.lineTo(x_val + 57, y_val);
+    ctx.lineTo(x_val + len, y_val);
     ctx.stroke();
 
     ctx.beginPath();
     ctx.setLineDash([]);
-    ctx.moveTo(x_val + 57, y_val);
-    ctx.lineTo(x_val + 57 - 7, y_val - 6);
+    ctx.moveTo(x_val + len, y_val);
+    ctx.lineTo(x_val + len - 7, y_val - 6);
     ctx.stroke();
 
     ctx.beginPath();
     ctx.setLineDash([]);
-    ctx.moveTo(x_val + 57, y_val);
-    ctx.lineTo(x_val + 57 - 7, y_val + 6);
+    ctx.moveTo(x_val + len, y_val);
+    ctx.lineTo(x_val + len - 7, y_val + 6);
     ctx.stroke();
   } else if (dir == "vertical") {
     ctx.beginPath();
     ctx.setLineDash([]);
     ctx.moveTo(x_val, y_val);
-    ctx.lineTo(x_val, y_val + 57);
+    ctx.lineTo(x_val, y_val + len);
     ctx.stroke();
 
     ctx.beginPath();
     ctx.setLineDash([]);
-    ctx.moveTo(x_val, y_val + 57);
-    ctx.lineTo(x_val - 6, y_val + 57 - 7);
+    ctx.moveTo(x_val, y_val + len);
+    ctx.lineTo(x_val - 6, y_val + len - 7);
     ctx.stroke();
 
     ctx.beginPath();
     ctx.setLineDash([]);
-    ctx.moveTo(x_val, y_val + 57);
-    ctx.lineTo(x_val + 6, y_val + 57 - 7);
+    ctx.moveTo(x_val, y_val + len);
+    ctx.lineTo(x_val + 6, y_val + len - 7);
     ctx.stroke();
   }
 }
@@ -103,21 +116,21 @@ export function list(
   x_val: number,
   y_val: number
 ) {
+  let count = lst.length;
   lst.forEach((e: any, i: number) => {
     ctx.strokeRect(x_val + 120 * i, y_val, 40, 40);
     ctx.strokeRect(x_val + 40 + 120 * i, y_val, 30, 40);
     ctx.beginPath();
     ctx.ellipse(x_val + 120 * i + 20, y_val + 20, 4, 4, 0, 0, 7);
     ctx.fill();
-    arrow(ctx, x_val + 120 * i + 20, y_val + 20, "vertical");
     if (typeof e === "object") {
       if (e.type === "list") {
-        list(ctx, e.value, x_val + 120 * i + 13, y_val + 84);
+        list(ctx, e.value, x_val + 120 * i + 13, y_val + 57 + 40*count);
       } else if (e.type === "vector") {
-        vector(ctx, e.value, x_val + 120 * i + 13, y_val + 97);
+        vector(ctx, e.value, x_val + 120 * i + 13, y_val + 57 + 40*count);
       }
     } else {
-      ctx.fillText(`${e}`, x_val + 120 * i + 13, y_val + 27 + 77);
+      ctx.fillText(`${e}`, x_val + 120 * i + 13, y_val + 57 + 40*count);
     }
 
     // draws dot and arrow to next list element
@@ -125,8 +138,7 @@ export function list(
       ctx.beginPath();
       ctx.ellipse(x_val + 120 * i + 55, y_val + 20, 4, 4, 0, 0, 7);
       ctx.fill();
-
-      arrow(ctx, x_val + 120 * i + 56, y_val + 20, "horizontal");
+      arrow(ctx, x_val + 120 * i + 56, y_val + 20, "horizontal", 57);
     } else {
       // draws the dash representing a null pointer
       ctx.beginPath();
@@ -135,6 +147,8 @@ export function list(
       ctx.lineTo(x_val + 70 + 120 * i, y_val + 40);
       ctx.stroke();
     }
+    arrow(ctx, x_val + 120 * i + 20, y_val + 20, "vertical", 10 + 40 * count);
+    count--;
   });
 }
 
@@ -144,34 +158,29 @@ export function vector(
   x_val: number,
   y_val: number
 ) {
-  let len_count = 0;
+  let count = vec.length;
   vec.forEach((e: any, i: number) => {
     let str_e = `${e}`;
     let str_i = `${i}`;
 
     ctx.font = "12px serif";
-    ctx.fillText(str_i, x_val + len_count, y_val - 3);
+    ctx.fillText(str_i, x_val + 40 * i, y_val - 3);
     ctx.font = "24px courier";
-
+    ctx.strokeRect(x_val + 40 * i, y_val, 40, 40);
     ctx.beginPath();
-    ctx.ellipse(x_val + len_count + 20, y_val + 20, 4, 4, 0, 0, 7);
+    ctx.ellipse(x_val + 40 * i + 20, y_val + 20, 4, 4, 0, 0, 7);
     ctx.fill();
-    arrow(ctx, x_val + len_count + 20, y_val + 20, "vertical");
     if (typeof e === "object") {
       if (e.type === "list") {
-        ctx.strokeRect(x_val + len_count, y_val, e.value.length * 120, 40);
-        list(ctx, e.value, x_val + len_count + 13, y_val + 84);
-        len_count += e.value.length * 120;
+        list(ctx, e.value, x_val + 40 * i + 13, y_val + 37 + 40 * count);
       } else if (e.type === "vector") {
-        ctx.strokeRect(x_val + len_count, y_val, e.value.length * 120, 40);
-        vector(ctx, e.value, x_val + len_count + 13, y_val + 97);
-        len_count += e.value.length * 120;
+        vector(ctx, e.value, x_val + 40 * i + 13, y_val + 37 + 40 * count);
       }
     } else {
-      ctx.strokeRect(x_val + len_count, y_val, 40 + 16 * str_e.length, 40);
-      ctx.fillText(str_e, x_val + len_count + 13, y_val + 27 + 77);
-      len_count += str_e.length * 16 + 40;
+      ctx.fillText(str_e, x_val + 40 * i + 13, y_val + 57 + 40 * count);
     }
+      arrow(ctx, x_val + 40 * i + 20, y_val + 20, "vertical", 10 + 40*count);
+      count--;
   });
 }
 
@@ -180,27 +189,17 @@ export function environment(ctx: CanvasRenderingContext2D, rows: any[]) {
   rows.forEach((e, i) => {
     ctx.strokeRect(7, 7 + i * 45 + buffer, 26 + 16 * e.name.length, 40);
 
-    arrow(ctx, 16 * e.name.length + 20, 26 + i * 45 + buffer, "horizontal");
+    arrow(ctx, 16 * e.name.length + 20, 26 + i * 45 + buffer, "horizontal", 57);
 
     ctx.font = "24px courier";
 
     ctx.fillText(`${e.name}`, 13, 32 + i * 45 + buffer);
     if (e.value.type === "list") {
       list(ctx, e.value.value, 16 * e.name.length + 88, 7 + i * 45 + buffer);
-      buffer += 80;
-      e.value.value.forEach((e: any) => {
-        if (typeof e === "object") {
-          buffer += 80;
-        }
-      });
+      buffer += 40*e.value.length;
     } else if (e.value.type === "vector") {
       vector(ctx, e.value.value, 16 * e.name.length + 88, 20 + i * 45 + buffer);
-      buffer += 80;
-      e.value.value.forEach((e: any) => {
-        if (typeof e === "object") {
-          buffer += 80;
-        }
-      });
+      buffer += 5340*e.value.length;
     } else {
       ctx.fillText(`${e.value}`, 16 * e.name.length + 88, 32 + i * 45 + buffer);
     }
@@ -212,6 +211,7 @@ export function Canvas(props: { width: number; height: number; rows: any[] }) {
 
   useEffect(() => {
     const canvas = canvasRef.current;
+    //@ts-expect-error
     const ctx = canvas.getContext("2d");
     const rows = props.rows;
     ctx.textRendering = "geometricPrecision";
@@ -242,7 +242,7 @@ export default function Test() {
   return (
     <div>
       <b>Scamper</b>
-      <Canvas width={1000} height={1000} rows={rows} />
+      <Canvas width={2000} height={2000} rows={rows} />
     </div>
   );
 }
